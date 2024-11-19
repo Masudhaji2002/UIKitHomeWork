ccb1786b2da59f19ff9658540ef48672b611866f
Masud11 <masud.qadjiev@list.ru>
2024-11-19 11:51:55 +0300

    review

--- UIKitPotok/SceneDelegate.swift
+++ UIKitPotok/SceneDelegate.swift
@@ -15,14 +15,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        // Создаем новое окно
+        
         let window = UIWindow(windowScene: windowScene)
         
-        // Задаем корневой контроллер (замените `YourRootViewController` на ваш контроллер)
         let rootViewController = ViewController()
         window.rootViewController = UINavigationController(rootViewController: rootViewController)
         
-        // Делаем окно видимым и активным
+        
         self.window = window
         window.makeKeyAndVisible()
     }
